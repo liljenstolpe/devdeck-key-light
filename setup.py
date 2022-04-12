1c383d335ac8b0464fcc8d0df87d80140937d8be
Christopher LILJENSTOLPE <cdl@asgaard.org>
2022-04-12 16:39:51 -0700

    changed the xdg config dir source

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 import subprocess
 
+from xdg.BaseDirectory import *
 
 def get_version():
     process = subprocess.Popen(["git", "describe", "--always", "--tags"], stdout=subprocess.PIPE, stderr=None)
@@ -10,6 +11,8 @@ def get_version():
     else:
         return last_tag
 
+assetsDir = os.path.join(xdg_config_home, 'devdeck/assets')
+print(assetsDir)
 
 with open('requirements.txt') as f:
     install_reqs = f.read().splitlines()
@@ -25,5 +28,6 @@ setup(
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=install_reqs
+    install_requires=install_reqs,
+    data_files=[(str(assetsDir), ['assets/key-light-on.png','assets/key-light-off.png'])]
 )
